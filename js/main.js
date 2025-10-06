4f7d55bc86293ed1aed0bfa553bf1fee7f690a3e
ㅤ1 <IntOflnxoverxdx>
2025-10-06 18:28:33 +1000

    готова вся основная логика для игры

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.onresize = () => {
+    document.querySelector("body").style.scale = `${window.innerWidth / 390}`
+}
+document.querySelector("body").style.scale = `${window.innerWidth / 390}`
--- js/runes.js
+++ js/runes.js
@@ -1,30 +1,28 @@
 
-levels = localStorage.getItem("levels")
-if (!levels){
-    levels = [[
-        {type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},
-        {type:"present",value:1,opened:false}
-    ].sort(function(){return Math.random() - 0.5;}),
-    [
-        {type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:8,opened:false},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false0},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},{type:"bat",value:1,opened:false5},
-        {type:"present",value:2,opened:false}
-    ].sort(function(){return Math.random() - 0.5;}),
-    ].sort(function(){return Math.random() - 0.5;})
-    
-    localStorage.setItem("levels",JSON.stringify(levels))
+info = localStorage.getItem("info")
+if (!info) {
+    info = {
+        field: [{ type: "bat", value: 5, opened: false }, { type: "bat", value: 5, opened: false }, { type: "bat", value: 5, opened: false }, { type: "bat", value: 5, opened: false }, { type: "bat", value: 5, opened: false }, { type: "bat", value: 5, opened: false }, { type: "bat", value: 5, opened: false }, { type: "bat", value: 5, opened: false }, { type: "bat", value: 5, opened: false }, { type: "bat", value: 5, opened: false }, { type: "present", value: 1, opened: false }, { type: "present", value: 2, opened: false }, { type: "present", value: 3, opened: false }, { type: "present", value: 4, opened: false }, { type: "present", value: 5, opened: false }, { type: "bat", value: 10, opened: false }, { type: "bat", value: 10, opened: false }, { type: "bat", value: 10, opened: false }, { type: "bat", value: 10, opened: false }, { type: "bat", value: 10, opened: false }, { type: "bat", value: 10, opened: false }, { type: "bat", value: 10, opened: false }, { type: "bat", value: 40, opened: false }, { type: "bat", value: 40, opened: false }, { type: "bat", value: 40, opened: false }].sort(function () { return Math.random() - 0.5; }),
+        shovels: 25,
+        bats: 0,
+        presents:[]
+    }
+
+    localStorage.setItem("info", JSON.stringify(info))
+    info = localStorage.getItem("info")
 }
-levels = JSON.parse(levels)
+info = JSON.parse(info)
+document.querySelector(".shovel-counter").innerHTML = `Лопаты: ${info.shovels}`
+document.querySelector(".bat-counter").innerHTML = `Мышки: ${info.bats}`
 
 
 
-levels[0].forEach
+info.field.forEach((item, i) => {
 
-levels[0].forEach((item,i) => {
-    
-    if (item.type == "bat"){
+    if (item.type == "bat") {
         document.querySelector(".game").innerHTML += `
-        <div class="rune ${item.opened?"actuve":""}">
-            <img src="img/runes/${i+1}.png" alt="" class="rune__img">
+        <div class="rune ${item.opened ? "active" : ""}">
+            <img src="img/runes/${i + 1}.png" alt="" class="rune__img">
             <div class="rune__bat">
                 <img src="img/icons/bat.png" alt="" class="rune__bat-img">
                 <div class="bat-text">${item.value}</div>
@@ -32,10 +30,10 @@ levels[0].forEach((item,i) => {
         </div>
         
         `
-    }else{
+    } else {
         document.querySelector(".game").innerHTML += `
-        <div class="rune ${item.opened?"actuve":""}">
-            <img src="img/runes/${i+1}.png" alt="" class="rune__img">
+        <div class="rune ${item.opened ? "active" : ""}">
+            <img src="img/runes/${i + 1}.png" alt="" class="rune__img">
             <div class="rune__present">
                 <img src="img/runes/present.png" alt="" class="rune__present-img">
             </div>
@@ -44,13 +42,44 @@ levels[0].forEach((item,i) => {
     }
 });
 
-document.querySelectorAll(".rune").forEach((rune,i) => {
-    rune.onclick = ()=>{
+document.querySelectorAll(".rune").forEach((rune, i) => {
+    rune.onclick = () => {
         rune.classList.add("active")
-        console.log(levels[0][i].opened);
-        levels[0][i].opened = true
-        
-        localStorage.setItem("levels",JSON.stringify(levels))
-        rune.onclick = ()=>{}
+
+        let newField = info.field
+        newField[i] = {
+            ...newField[i],
+            opened: true
+        }
+
+        let plusBats = 0
+        if (info.field[i].type == "bat") {
+            plusBats = info.field[i].value
+        }
+
+        info = {
+            ...info,
+            field: newField,
+            shovels: info.shovels - 1,
+            bats: info.bats + plusBats
+        }
+
+        document.querySelector(".shovel-counter").innerHTML = `Лопаты: ${info.shovels}`
+        document.querySelector(".bat-counter").innerHTML = `Мышки: ${info.bats}`
+
+
+        localStorage.setItem("info", JSON.stringify(info))
+        rune.onclick = () => { }
     }
 });
+
+
+
+
+
+
+
+function reboot() {
+    localStorage.clear()
+    window.location.reload()
+}
