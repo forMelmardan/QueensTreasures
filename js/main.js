07910ce45e3bc5684326b2d801f735fc27e80155
IntOflnxoverxdx <LLLLLLLLLLLLLLirik@gmail.com>
2025-10-10 00:31:37 +1000

    Добавлен занавес

--- js/main.js
+++ js/main.js
@@ -1,4 +1,33 @@
-document.onresize = () => {
-    document.querySelector("body").style.scale = `${window.innerWidth / 390}`
+let position = { x: window.pageXOffset, y: window.pageYOffset }
+document.onscroll = () => {
+    window.scrollTo(position.x, position.y)
 }
-document.querySelector("body").style.scale = `${window.innerWidth / 390}`
+
+
+window.onresize = () => {
+    setSizes()
+}
+setSizes()
+function setSizes() {
+    document.querySelector("main").style.scale = window.innerWidth / 390
+    if (window.innerWidth >= window.innerHeight) {
+        document.querySelectorAll(".curtain-back").forEach(e => {
+            e.style = `width:100%;`
+        })
+    } else {
+        document.querySelectorAll(".curtain-back").forEach(e => {
+            e.style = `height:100%;`
+        })
+    }
+}
+
+
+window.onload = () => {
+    document.querySelectorAll(".curtain-back, .curtain-front").forEach(e => {
+        e.classList.add("fade-Out")
+    });
+    setTimeout(() => {
+        document.querySelector(".curtain").remove()
+        document.onscroll = ()=>{}
+    }, 3000);
+}
