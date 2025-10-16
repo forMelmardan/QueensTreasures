b884c7b58c998bbb30b6a69a38ab95b0fdf79d10
IntOflnxoverxdx <LLLLLLLLLLLLLLirik@gmail.com>
2025-10-17 07:43:18 +1000

    Добавил ошибку на выключенный сервер

--- js/bot.js
+++ js/bot.js
@@ -1,22 +1,28 @@
 async function sendTelegramMessage(text) {
     let res = false
-    await fetch("https://6d1f294a-14fa-441a-b812-67da83a85f2e-00-1cx2smbm43ipu.worf.replit.dev/send-message", {
-        method: "POST",
-        headers: {
-            "content-type": "application/json"
-        },
-        body: JSON.stringify({
-            content: text
+    try{
+        await fetch("https://6d1f294a-14fa-441a-b812-67da83a85f2e-00-1cx2smbm43ipu.worf.replit.dev/send-message", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({
+                content: text
+            })
+        }).then(r => {
+            if (r.status == 200) {
+                res = true
+            }
+            
         })
-    }).then(r => {
-        if (r.status == 200) {
-            res = true
+        if (res) {
+            alert("Успешно!")
+        } else {
+            alert("Технические шоколадки.... Моя любовь, попробуй через пару минуток!!")
         }
-    })
-    if (res) {
-        alert("Успешно!")
-    } else {
-        alert("Технические шоколадки.... Моя любовь, попробуй через пару минуток!!")
+        return res
+    }catch{
+        alert("Шоколадки технические немножко!! Сообщи пожалуйста мне, я сейчас починю!")
+        return false
     }
-    return res
 }
